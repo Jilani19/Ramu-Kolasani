82648765028bf045bc7a6b9d9c6efb29ec6ec040
Jilani19 <jilanik290713@gmail.com>
2026-05-09 01:34:30 +0530

    Update leader name from John Kumar to Ramu Kalasani

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -23,7 +23,7 @@ function About() {
               alt="Leader"
             />
 
-            <h2>John Kumar</h2>
+            <h2>Ramu Kalasani</h2>
 
             <p>
               Dedicated Leader Serving the Community
